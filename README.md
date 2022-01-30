8744d45951acbd246c9d81043d39b95204ca5d04
TheBrunoRM <bruno12345best@gmail.com>
2022-01-30 05:18:10 -0300

    1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # memedroid
+
+Example usage
+
+```js
+const memedroid = require("memedroid");
+
+// you can specify any valid memedroid URL
+// examples:
+// https://es.memedroid.com/memes/top/day
+// https://es.memedroid.com/memes/top/week
+// https://es.memedroid.com/memes/top/month
+// https://es.memedroid.com/memes/top/ever
+// https://es.memedroid.com/memes/random
+
+// you can also add a page number to the end of the url
+// example: https://es.memedroid.com/memes/top/day/5
+
+memedroid.getMemes(`https://es.memedroid.com/memes/latest`)
+    .then(memes => console.log(memes));
+```
+
+Example response
+
+```js
+[{
+    id: '9999999',
+    name: 'This is a post',
+    author: 'HelloWorld',
+    rating: 95, // percentage
+    votes: 913,
+    positiveVotes: 881,
+    link: 'https://es.memedroid.com/memes/detail/9999999',
+    date: "2077-01-01T00:00:00.000Z",
+    poster: 'https://memedroid.com/videos/image.jpeg',
+    video: [{
+        url: "https://memedroid.com/videos/video.mp4",
+        type: "video/mp4",
+    }],
+    image: [
+        'https://memedroid.com/images/image.jpeg',
+    ]
+}]
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const fetch = require("node-fetch");
+
+// NOTE: shift() is used to remove the code that goes before the useful information
+module.exports.getMemes = async (url = `https://es.memedroid.com/memes/top/day`) => {
+    let data = await fetch(url).then(d => d.text());
+    let memes = data.split(`<div class="gallery-memes-container">`)[1].split(`article id=`);
+    memes.shift();
+    return memes.map(meme => {
+
+        let video = false;
+        if (meme.includes(`<div class="video-container"`)) {
+            video = meme.split("<source src=").map(src => {
+                let url = src.split(`"`)[1].split(`"`)[0];
+                let type = src.split(`type="`)[1].split(`">`)[0];
+                return { url, type };
+            });
+            video.shift();
+        }
+
+        let image = meme.split(`<img src=`).map(src => {
+            return src.split(`"`)[1].split(`"`)[0];
+        });
+        image.shift();
+
+        let id = meme.split(`"article-item-`)[1]?.split(`"`)[0];
+        let name = meme.split(`href="`)[1]?.split(`">`)[1]?.split(`</a>`)[0];
+        let author = meme.split(`By <a`)[1]?.split(`">`)[1]?.split(`</a>`)[0];
+        let rating = parseInt(meme.split(`<div class="item-rating-container"`)[1]?.split(`>`)[2]?.split(`<`)[0].replace("%", ""));
+        let votes = parseInt(meme.split(`data-votes="`)[1]?.split(`"`)[0]);
+        let positiveVotes = parseInt(meme.split(`data-positive-votes="`)[1].split(`"`)[0]);
+        let link = meme.split(`data-share-link="`)[1]?.split(`"`)[0];
+        let date = new Date(meme.split(`<time datetime="`)[1]?.split(`"`)[0]);
+        let poster = meme.split(`poster="`)[1]?.split(`"`)[0];
+
+        return { id, name, author, rating, votes, positiveVotes, link, date, poster, video, image }
+    });
+}
